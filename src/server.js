d8b10330aaa5b0e7714550d8010c9543ff00e648
JJMior <jonnates.mior@gmail.com>
2020-09-08 18:59:21 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,81 @@
+const express = require("express")
+const server = express()
+
+
+// PEGAR O BANCO DE DADOS
+const db = require("./database/db")
+
+
+// CONFIGURAR PASTA PUBLIC
+server.use(express.static("public"))
+
+// habilitar o uso do req.body na aplicação
+server.use(express.urlencoded({ extended: true}))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+server.get("/create-point", (req, res) => {
+
+    return res.render("create-point.html")
+})
+server.post("/savepoint", (req, res) => {
+    // console.log(req.body)
+    const query = `
+            INSERT INTO places (
+                image,
+                name,
+                address,
+                address2,
+                state,
+                city,
+                items
+            ) VALUES (?,?,?,?,?,?,?);    
+        `
+    const values = [
+            req.body.image,
+            req.body.name,
+            req.body.address,
+            req.body.address2,
+            req.body.state,
+            req.body.city,
+            req.body.items
+        ]
+    
+    function afterInsertData(err) {
+            if(err) {
+                console.log(err)
+                return res.send("Erro no cadastro!")
+            }
+            console.log("Cadastrado com sucesso")
+            console.log(this)
+            return res.render("create-point.html", {saved: true })
+        }
+        db.run(query, values, afterInsertData)    
+})
+
+server.get("/search", (req, res) => {
+
+    const search = req.query.search
+    if(search == "") {
+        return res.render("search-results.html", { total: 0})
+    }
+
+
+    // PAGAR OS DADOS DO BANCO DE DADOS
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+                if(err) {
+                    return console.log(err)
+                }
+                const total = rows.length
+                // mostrar a pagina html com os dados do banco de cados
+                return res.render("search-results.html", { places: rows, total })
+            })    
+})
+server.listen(3000)
